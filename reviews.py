1d6bb6345dd9fc3e90f2a8e9c633a8f278fa1a4f
Derek Li <derek.j.h.li@gmail.com>
2020-10-20 22:58:47 +0800

    updated the code

--- reviews.py
+++ reviews.py
@@ -3,14 +3,13 @@ with open("reviews.txt", "r") as f :
 	for line in f :
 		data.append(line)
 		# 每讀一千筆 印出
-		if len(data) % 1000 == 0 :
-			print(len(data))
+		# if len(data) % 1000 == 0 :
+		# 	print(len(data))
 print("檔案讀取完了", len(data), "筆資料")
 
 # 算留言平均長度
-data_length = []
-with open("reviews.txt", "r") as f :
-	for line in f :
-		data_length.append(len(line))
-avr_reviews = sum(data_length) / len(data_length)
-print(f"留言平均長度為{avr_reviews}")
+# 更新code
+sum_len = 0
+for line in data :
+	sum_len += len(line)
+print(f'留言平均長度為{sum_len/len(data)}')
