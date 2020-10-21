bc135c06cb6f472bcacb32488953cd37375ac63c
Derek Li <derek.j.h.li@gmail.com>
2020-10-21 22:46:07 +0800

    add filter function

--- reviews.py
+++ reviews.py
@@ -3,8 +3,8 @@ with open("reviews.txt", "r") as f :
 	for line in f :
 		data.append(line)
 		# 每讀一千筆 印出
-		# if len(data) % 1000 == 0 :
-		# 	print(len(data))
+		if len(data) % 1000 == 0 :
+			print(len(data))
 print("檔案讀取完了", len(data), "筆資料")
 
 # 算留言平均長度
@@ -21,3 +21,10 @@ for line in data :
 max_num = max(data_max)
 print(max_num)
 print(data[data_max.index(max_num)])
+
+# 篩選留言字數少於100字
+data_filter = []
+for line in data:
+	if len(line) < 100 :
+		data_filter.append(line)
+print(f"一共有{len(data_filter)}筆資料留言少於100字")
