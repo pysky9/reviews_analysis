a569ab755494579d6dbf0dfc7fb742a75289c308
Derek Li <derek.j.h.li@gmail.com>
2020-11-07 15:02:33 +0800

    add the counting and searching

--- reviews.py
+++ reviews.py
@@ -1,3 +1,4 @@
+import time
 data = []
 with open("reviews.txt", "r") as f :
 	for line in f :
@@ -35,3 +36,28 @@ for d in data :
 	if 'good' in d:
 		good.append(d)
 print(f"一共有{len(good)}筆留言提到GOOD")
+
+# 讓使用者查詢
+start_time = time.time()
+wc = {} # word count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else :
+			wc[word] = 1
+end_time = time.time() 
+run_time = end_time - start_time
+print("運算時間為", run_time, "秒")
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+while True:
+	word = input("請輸入查詢的字:")
+	print("你查詢的", word, "出現", wc[word], "次")
+	q = input("請問您還要查詢嗎(y/n):")
+	if q.lower() == "n":
+		print("謝謝你的查詢")
+		break
